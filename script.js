90ad56ff2397b111bf6ec882ae883578164c0eb9
kathir-kingsmaker <kathirashwath@gmail.com>
2026-06-27 17:05:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const photos = [
+    "images/photo1.jpg",
+    "images/photo2.jpg",
+    "images/photo3.jpg",
+    "images/photo4.jpg"
+];
+
+let index = 0;
+
+setInterval(() => {
+    index = (index + 1) % photos.length;
+    document.getElementById("slideshow").src = photos[index];
+}, 3000);
+
 document.getElementById("startBtn").addEventListener("click", function () {
-    alert("❤️ Happy Birthday My Love! ❤️\n\nThis website was made especially for you.");
+    alert("❤️ Happy Birthday My Love ❤️");
 });
